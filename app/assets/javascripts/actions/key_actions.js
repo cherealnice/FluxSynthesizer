f6677deadd8c27be0aafc494ebff9b680cae1c1d
Dan Cherouny <dcherouny@gmail.com>
2015-12-02 14:26:04 -0500

    Add styling to body and button up CAPS listener

--- app/assets/javascripts/actions/key_actions.js
+++ app/assets/javascripts/actions/key_actions.js
@@ -2,14 +2,14 @@ window.KeyActions = {
 
   keyPressed: function (note) {
     AppDispatcher.dispatch({
-      eventType: KeyConstants.KEY_PRESSED,
+      actionType: KeyConstants.KEY_PRESSED,
       note: note
     });
   },
 
   keyReleased: function (note) {
     AppDispatcher.dispatch({
-      eventType: KeyConstants.KEY_RELEASED,
+      actionType: KeyConstants.KEY_RELEASED,
       note: note
     });
   },
